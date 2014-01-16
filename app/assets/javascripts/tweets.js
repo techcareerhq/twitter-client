e2a5a75bc96a41b5131f9341b6af04e854d8d500
JesterMan <seanglang@gmail.com>
2014-01-15 19:28:25 -0800

    trying to get hastags up

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -2,6 +2,25 @@ $.ajax({
   url: "/api/retrieveTweets/abcd",
   type: "GET",
   success: function(response) {
-    debugger;
+    $.each(response.statuses, function(key, value){
+    	$(".container")
+    	.append("<b>"+value.text+"</b><br>")
+    	.append("Created on: "+value.created_at+"<br>")
+    	.append("<a href ='https://twitter.com/"+value.user.screen_name+"/statuses/"+value.id_str+"'>Link to tweet</a><br>")
+		.append(value.user.name+"<br>")
+		.append("Followers: "+value.user.followers_count+"<br>")
+		.append("Retweeted: "+value.retweet_count+"<br>")
+		.append("<a href='http://twitter.com/"+value.user.screen_name+"'>"+value.user.screen_name+"</a><br><br>");
+		// $.each(user.entities.hashtags, function(key, value){
+		// 	.append("#"+value.text+"    ");
+		// });
+		if (value.in_reply_to_status_id){
+			$(".container").append("<span class='response'>In response to: <a href ='https://twitter.com/"
+    			+value.in_reply_to_user_id_str+"/statuses/"+value.in_reply_to_status_id_str="'>"
+    			+value.in_reply_to_user_id_str+"</a></span>");
+
+		}
+		
+    });
   }
 })
