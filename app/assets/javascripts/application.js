ca2a9707256a9db9f0a02139d5655d6ee80cf825
JesterMan <seanglang@gmail.com>
2014-01-20 19:09:57 -0800

    got rid of html text

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,4 @@
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -12,4 +13,6 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require_tree ../templates
 //= require_tree .
+//= require list.min
--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,30 +1,35 @@
+var template = JST["tweet"]
 $.ajax({
   url: "/api/retrieveTweets/abcd",
   type: "GET",
   success: function(response) {
     $.each(response.statuses, function(key, value){
-    	$(".container")
-    	.append("<b>"+value.text+"</b><br>")
-    	.append("Created on: "+value.created_at+"<br>")
-    	.append("<a href ='https://twitter.com/"+value.user.screen_name+"/statuses/"+value.id_str+"'>Link to tweet</a><br>")
-		.append(value.user.name+"<br>")
-		.append("Followers: "+value.user.followers_count+"<br>")
-		.append("Retweeted: "+value.retweet_count+"<br>")
-		.append("<a href='http://twitter.com/"+value.user.screen_name+"'>"+value.user.screen_name+"</a><br>");
-		// $.each(user.entities.hashtags, function(key, value){
-		// 	.append("#"+value.text+"    ");
-		// });
-		if (value.in_reply_to_status_id){
-			$(".container").append("<span class='response'>In response to: <a href ='https://twitter.com/"
-    			+value.in_reply_to_user_id_str+"/statuses/"+value.in_reply_to_status_id_str="'>"
-    			+value.in_reply_to_user_id_str+"</a></span>");
+  //   	$(".container")
+  //   	.append("<b>"+value.text+"</b><br>")
+  //   	.append("Created on: "+value.created_at+"<br>")
+  //   	.append("<a href ='https://twitter.com/"+value.user.screen_name+"/statuses/"+value.id_str+"'>Link to tweet</a><br>")
+		// .append(value.user.name+"<br>")
+		// .append("Followers: "+value.user.followers_count+"<br>")
+		// .append("Retweeted: "+value.retweet_count+"<br>")
+		// .append("<a href='http://twitter.com/"+value.user.screen_name+"'>"+value.user.screen_name+"</a><br>");
 
-		}
-		// if (response.statuses.entities.media){
-		// 	$(".container").append("<img src='"+value.value.entities.media.media_url+"'/>");
+		// $.each(value.entities.hashtags, function(ID, hashWord){
+  //     $('.container').append("<b>#"+hashWord.text+"</b>    ");
+  //   });
+		// if (value.in_reply_to_status_id){
+		// 	$(".container").append("<span class='response'>In response to: <a href ='https://twitter.com/"
+  //   			+value.in_reply_to_user_id_str+"/statuses/"+value.in_reply_to_status_id_str="'>"
+  //   			+value.in_reply_to_user_id_str+"</a></span>");
 
 		// }
-		$(".container").append("<br><br>")
+		
+		// if ('media' in value.entities){
+			
+		// 	 $(".container").append("<img src='"+value.entities.media[0].media_url+"' />");
+
+		// }
+		// $(".container").append("<br><br>")
+		$(".container").append(template({status: value}));
 		
     });
   }
