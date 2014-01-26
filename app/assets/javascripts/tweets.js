536b4ca89a3406145f73418cc12ee2676247ceb1
JesterMan <seanglang@gmail.com>
2014-01-26 12:36:07 -0800

    working with list

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -3,34 +3,21 @@ $.ajax({
   url: "/api/retrieveTweets/abcd",
   type: "GET",
   success: function(response) {
+
+  	var options = {
+  valueNames: [ 'name', 'retweeted', 'created', 'followers', 'influence' ]
+};
+var userList = new List('users', options);
+	
+		$(".container").append("<input class='search' placeholder='Search' />");
+  	$(".container").append("<div id='users'>");
+  	$(".container").append("<button class='sort' data-sort='name'>Sort by name/author</button>");
+  	$(".container").append("<button class='sort' data-sort='retweeted'>Sort by retweets</button>");
+  	$(".container").append("<button class='sort' data-sort='created'>Sort by date</button>");
+  	$(".container").append("<button class='sort' data-sort='followers'>Sort by followers</button>");
+  	$(".container").append("<button class='sort' data-sort='influence'>Sort by influence</button></div>");
     $.each(response.statuses, function(key, value){
-  //   	$(".container")
-  //   	.append("<b>"+value.text+"</b><br>")
-  //   	.append("Created on: "+value.created_at+"<br>")
-  //   	.append("<a href ='https://twitter.com/"+value.user.screen_name+"/statuses/"+value.id_str+"'>Link to tweet</a><br>")
-		// .append(value.user.name+"<br>")
-		// .append("Followers: "+value.user.followers_count+"<br>")
-		// .append("Retweeted: "+value.retweet_count+"<br>")
-		// .append("<a href='http://twitter.com/"+value.user.screen_name+"'>"+value.user.screen_name+"</a><br>");
-
-		// $.each(value.entities.hashtags, function(ID, hashWord){
-  //     $('.container').append("<b>#"+hashWord.text+"</b>    ");
-  //   });
-		// if (value.in_reply_to_status_id){
-		// 	$(".container").append("<span class='response'>In response to: <a href ='https://twitter.com/"
-  //   			+value.in_reply_to_user_id_str+"/statuses/"+value.in_reply_to_status_id_str="'>"
-  //   			+value.in_reply_to_user_id_str+"</a></span>");
-
-		// }
-		
-		// if ('media' in value.entities){
-			
-		// 	 $(".container").append("<img src='"+value.entities.media[0].media_url+"' />");
-
-		// }
-		// $(".container").append("<br><br>")
-		$(".container").append(template({status: value}));
-		
+		$(".container").append(template({status: value}));		
     });
   }
 })
