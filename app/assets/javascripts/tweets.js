9579a7db63cb91411867b00e7fb8c9c1b853caac
JesterMan <seanglang@gmail.com>
2014-01-30 17:40:18 -0800

    took out hiding options, fied li bullets

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,4 +1,6 @@
 var template = JST["tweet"]
+	topTemplate = JST["headsup"]
+
 
 $.ajax({
   url: "/api/retrieveTweets/abcd",
@@ -9,18 +11,6 @@ $.ajax({
   		valueNames: [ 'name', 'retweeted', 'created', 'followers', 'influence' ]
 	};
 
- 	$(".container").append("Click to toggle:");
- 	$(".container").append("<label for='name-toggle'>Name</label>  ");
-	$(".container").append("<input type='checkbox' id='name-toggle'/> ");
-	$(".container").append("<label for='retweet-toggle'>Retweets</label> "); 
-	$(".container").append("<input type='checkbox' id='retweet-toggle'/> ");
-	$(".container").append("<label for='date-toggle'>Date</label>  ");
-	$(".container").append("<input type='checkbox' id='date-toggle'/> ");
-	$(".container").append("<label for='followers-toggle'>Followers</label>  ");
-	$(".container").append("<input type='checkbox' id='followers-toggle'/> ");
-	$(".container").append("<label for='influence-toggle'>Influence</label>");  
-	$(".container").append("<input type='checkbox' id='influence-toggle'/>"); 
-	$(".container").append("<label for='influence-toggle'>Influence</label>");  
 	$(".container").append("<div id='users'></div>");
 	$("#users").append("<button class='sort btn' data-sort='name'>Sort by name/author</button>");
 	$("#users").append("<button class='sort' data-sort='retweeted'>Sort by retweets</button>");
